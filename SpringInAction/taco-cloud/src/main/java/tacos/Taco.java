b44d99f808411ac6d358bc8216fd66ee115ed397
girawhale <girawhale@naver.com>
2021-02-09 03:40:00 +0900

    21.02.09 JavaInAction5 3.1 JDBC 보는중....

--- SpringInAction/taco-cloud/src/main/java/tacos/Taco.java
+++ SpringInAction/taco-cloud/src/main/java/tacos/Taco.java
@@ -4,15 +4,18 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
+    private Long id;
+    private Date createdAt;
 
     @NotNull
     @Size(min = 5, message = "Name은 최소 5자 이상이어야 합니다.")
     private String name;
 
     @Size(min = 1, message = "최소 재료는 1개 이상입니다.")
-    private List<String> ingredients;
+    private List<Ingredient> ingredients;
 }
