5e69972f72f11a048360fa61b648b91081016fa3
girawhale <girawhale@naver.com>
2021-02-09 20:39:09 +0900

    21.02.09 SpringInAction5 3.1 JDBCTemplate 완료

--- SpringInAction/taco-cloud/src/main/java/tacos/data/JdbcTacoRepository.java
+++ SpringInAction/taco-cloud/src/main/java/tacos/data/JdbcTacoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 import tacos.Ingredient;
 import tacos.Taco;
 
@@ -13,6 +14,7 @@ import java.sql.Types;
 import java.util.Arrays;
 import java.util.Date;
 
+@Repository
 public class JdbcTacoRepository implements TacoRepository {
     private JdbcTemplate jdbc;
 
